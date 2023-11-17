e0abae603ffa0ae9ce7bf95370d4b926634c3c41
Juan Ceballos <juandavi789@hotmail.com>
2023-11-16 23:44:45 -0500

    Add state to the app so we are now holding an array with 10 randon numbers, and every time the user clicks the roll button we set de dices to a new array with 10 random numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,44 @@
 import Die from './components/Die';
 import './App.css';
+import { useState } from 'react';
 
 const App = () => {
   const randomNumber = () => Math.floor(Math.random() * (6 - 1 + 1) + 1);
 
+  const allNewDice = () => {
+    const dicesArr = [];
+    for (let i = 0; i < 10; i++) {
+      dicesArr.push(randomNumber());
+    }
+    return dicesArr;
+  };
+
+  const rollDice = () => {
+    setDice(allNewDice);
+  };
+
+  const [dice, setDice] = useState(allNewDice());
+
   return (
     <div className='container'>
       <main className='game-container'>
-        <div className='dies-container'>
-          <Die value={randomNumber()} />
-          <Die value={randomNumber()} />
-          <Die value={randomNumber()} />
-          <Die value={randomNumber()} />
-          <Die value={randomNumber()} />
-          <Die value={randomNumber()} />
-          <Die value={randomNumber()} />
-          <Die value={randomNumber()} />
-          <Die value={randomNumber()} />
-          <Die value={randomNumber()} />
+        <div>
+          <h1>Tenzies</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam
+            mollitia dolorem, deserunt autem libero temporibus modi voluptatum.
+            Architecto a unde aspernatur itaque aut deleniti laborum sed non
+            corrupti natus! Eos!
+          </p>
         </div>
+        <div className='dies-container'>
+          {dice.map((die, i) => (
+            <Die key={i} value={die} />
+          ))}
+        </div>
+        <button className='cta' onClick={rollDice}>
+          Roll
+        </button>
       </main>
     </div>
   );
