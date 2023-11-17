b3d7fcc98c5b570d0340768f6fffa0c99cdce487
Juan Ceballos <juandavi789@hotmail.com>
2023-11-16 22:43:43 -0500

    Rendering 10 Die components manually with a randon number between 1 and 6 inclusive

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import Die from './components/Die';
 import './App.css';
 
 const App = () => {
+  const randomNumber = () => Math.floor(Math.random() * (6 - 1 + 1) + 1);
+
   return (
     <div className='container'>
-      <main className='game-container'></main>
+      <main className='game-container'>
+        <div className='dies-container'>
+          <Die value={randomNumber()} />
+          <Die value={randomNumber()} />
+          <Die value={randomNumber()} />
+          <Die value={randomNumber()} />
+          <Die value={randomNumber()} />
+          <Die value={randomNumber()} />
+          <Die value={randomNumber()} />
+          <Die value={randomNumber()} />
+          <Die value={randomNumber()} />
+          <Die value={randomNumber()} />
+        </div>
+      </main>
     </div>
   );
 };
