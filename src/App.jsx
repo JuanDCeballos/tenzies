4af042e8c75ab49663eee85ac549741df93b109c
Juan Ceballos <juandavi789@hotmail.com>
2023-11-18 22:09:10 -0500

    Add the functionality to hold a die on every roll click, the user is now able to hold a value for the rest of the game, while searching for the same numbers on every click

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,36 @@ import { nanoid } from 'nanoid';
 const App = () => {
   const randomNumber = () => Math.floor(Math.random() * (6 - 1 + 1) + 1);
 
+  const generateNewDie = () => {
+    return {
+      id: nanoid(),
+      value: randomNumber(),
+      isHeld: false,
+    };
+  };
+
   const allNewDice = () => {
     const dicesArr = [];
     for (let i = 0; i < 10; i++) {
-      const diceObj = {
-        id: nanoid(),
-        value: randomNumber(),
-        isHeld: false,
-      };
-      dicesArr.push(diceObj);
+      dicesArr.push(generateNewDie());
     }
     return dicesArr;
   };
 
   const rollDice = () => {
-    setDice(allNewDice());
+    setDice((prevDice) =>
+      prevDice.map((die) => {
+        return die.isHeld ? die : generateNewDie();
+      })
+    );
+  };
+
+  const holdDice = (id) => {
+    setDice((prevDice) =>
+      prevDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
   };
 
   const [dice, setDice] = useState(allNewDice());
@@ -28,18 +43,21 @@ const App = () => {
   return (
     <div className='container'>
       <main className='game-container'>
-        <div>
-          <h1>Tenzies</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam
-            mollitia dolorem, deserunt autem libero temporibus modi voluptatum.
-            Architecto a unde aspernatur itaque aut deleniti laborum sed non
-            corrupti natus! Eos!
+        <div className='game-description'>
+          <h1 className='title'>Tenzies</h1>
+          <p className='instructions'>
+            Roll until all dice are the same. Click each die to freeze it at its
+            current value between rolls.
           </p>
         </div>
         <div className='dies-container'>
           {dice.map((die) => (
-            <Die key={die.id} value={die.value} isHeld={die.isHeld} />
+            <Die
+              key={die.id}
+              value={die.value}
+              isHeld={die.isHeld}
+              holdDice={() => holdDice(die.id)}
+            />
           ))}
         </div>
         <button className='cta' onClick={rollDice}>
